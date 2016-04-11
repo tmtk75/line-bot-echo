81b468f9b1fb40169c63e6fd9827b7cc1cdf6a37
Tomotaka Sakuma <ktmtmks@gmail.com>
2016-04-11 23:02:10 +0900

    Integration with Kii servercode

--- heroku.tf
+++ heroku.tf
@@ -1,6 +1,6 @@
 variable "heroku_email" {}
 variable "heroku_api_key" {}
-variable "app_name" { default = "line-bot-air2phar" }
+variable "app_name" { default = "line-bot-eeh1taex" }
 variable "line_client_id"             {}
 variable "line_channel_secret"        {}
 variable "line_trusted_user_with_acl" {}
@@ -15,7 +15,7 @@ resource "heroku_app" "line-bot" {
     region = "us"
 
     config_vars {
-        LINE_CLIENT_ID             = "${var.line_client_id     }"
+        LINE_CLIENT_ID             = "${var.line_client_id}"
         LINE_CHANNEL_SECRET        = "${var.line_channel_secret}"
         LINE_TRUSTED_USER_WITH_ACL = "${var.line_trusted_user_with_acl}"
     }
--- main.js
+++ main.js
@@ -16,26 +16,35 @@ app.use(logger('dev'));
 
 app.post('/', (req, res) => {
   //console.log(req.body);
-  var msg = req.body.result[0];
-  console.log(msg);
-  fetch('https://trialbot-api.line.me/v1/events', {
-    method: "POST",
+  var app_id  = process.env.KII_APP_ID;
+  var app_key = process.env.KII_APP_KEY;
+  fetch('https://api-jp.kii.com/api/apps/' + app_id + "/server-code/versions/current/main", {
+    method: 'POST',
     headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-      'X-Line-ChannelID': process.env.LINE_CLIENT_ID,
-      'X-Line-ChannelSecret': process.env.LINE_CHANNEL_SECRET,
-      'X-Line-Trusted-User-With-ACL': process.env.LINE_TRUSTED_USER_WITH_ACL,
+      "content-type": "application/json",
+      "x-kii-appid": app_id,
+      "x-kii-appkey": app_key,
     },
-    body: JSON.stringify({
-      to: [msg['content']['from']],
-      toChannel: 1383378250,  // Fixed value
-      eventType: "138311608800106203",  // Fixed value
-      content: msg['content']
-    }),
+    body: JSON.stringify(req.body),
   })
-  .then((res) => res.text())
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res.returnedValue)
+    return fetch('https://trialbot-api.line.me/v1/events', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'X-Line-ChannelID': process.env.LINE_CLIENT_ID,
+        'X-Line-ChannelSecret': process.env.LINE_CHANNEL_SECRET,
+        'X-Line-Trusted-User-With-ACL': process.env.LINE_TRUSTED_USER_WITH_ACL,
+      },
+      body: JSON.stringify(res.returnedValue),
+    })
+    .then((res) => res.text())
+  })
   .then((res) => console.log(res))
   .catch((err) => console.log(err));
+
   res.status(201).send();
 });
 
